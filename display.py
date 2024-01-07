242d2c452d15ea1b2e91fd7d22c556f00b6ecab4
KISHAN KUMAR <kmali4551@gmail.com>
2024-01-08 00:20:03 +0530

    Remove extra space and give different colors for different types of cell

--- display.py
+++ display.py
@@ -7,15 +7,16 @@ def display_maze(maze):
         for col in range(total_columns):
 
             if (row == 0 and col == 0):
-                print(colored(" S  ","green","on_black"),end="  ")  
+                print(colored(" S ","green","on_black"),end="")  
             elif (row == total_rows-1 and col == total_columns-1):
-                print(colored(" E ","green","on_black"),end=" ")  
+                print(colored(" E ","green","on_black"),end="")  
             elif maze[row][col] == "◌":
-                print(colored(" ◌ ","blue","on_white"),end=" ")
+                print(colored(" ◌ ","blue","on_white"),end="")
             elif maze[row][col] == "▓":
-                print(colored(" ▓  ","red","on_white"),end=" ")
+                print(colored(" ▓ ","red","on_white"),end="")
             elif maze[row][col] == "◍":
-                print(colored(" ◍ ","green","on_black"),end="  ")
+                print(colored(" ◍ ","green","on_black"),end="")
             
 
         print()
+
